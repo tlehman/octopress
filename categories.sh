2c1c1c860cdc7b1692494c7a2ab09499a5f0db18
Tobi Lehman <tobi.lehman@gmail.com>
2013-12-08 11:59:11 -0800

    lowercase in categories.sh; add post Time Machine any dest

--- categories.sh
+++ categories.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git grep 'categories: \[' | perl -n -e '/\[([^\]]+)\]/ && print "$1\n"' | tr ', ' '\n' | awk NF | sort | uniq -c 
+git grep 'categories: \[' | perl -n -e '/\[([^\]]+)\]/ && print "$1\n"' | tr ', ' '\n' | awk NF | tr '[A-Z]' '[a-z]' | sort | uniq -c 
